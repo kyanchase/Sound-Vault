545952525d223128d4e463e9d8da3668a79f9743
kyanchase <165103120+kyanchase@users.noreply.github.com>
2024-12-20 23:43:57 -0500

    Initial Commit

---
+++ Sound Vault/Sound_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_VaultApp.swift
+//  Sound Vault
+//
+//  Created by Kyan Chase on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Sound_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sound VaultTests/Sound_VaultTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_VaultTests.swift
+//  Sound VaultTests
+//
+//  Created by Kyan Chase on 12/20/24.
+//
+
+import Testing
+@testable import Sound_Vault
+
+struct Sound_VaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
